8dc7183af5e06324cc9e9fcac3426b04cce13988
xyler0 <130307509+xyler0@users.noreply.github.com>
2025-11-11 17:34:53 +0100

    chore: remove old root push-service folder

--- push-service/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { PushModule } from './push/push.module';
-
-@Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, PushModule],
-})
-export class AppModule {}
--- push-service/src/push/push.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PushService } from './push.service';
-import { PushController } from './push.controller';
-
-@Module({
-  providers: [PushService],
-  controllers: [PushController]
-})
-export class PushModule {}
