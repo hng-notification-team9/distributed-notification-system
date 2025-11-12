131772ff8e5ffc4075ea8de5c8a6fe8110e88957
josseycodes1 <adewumijosephine1@gmail.com>
2025-11-12 19:42:23 +0100

    Feat:Services - use passive queue declaration

--- api_gateway/settings.py
+++ api_gateway/settings.py
@@ -182,11 +182,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# # Security settings for production
-# if not DEBUG:
-#     SECURE_SSL_REDIRECT = True
-#     SESSION_COOKIE_SECURE = True
-#     CSRF_COOKIE_SECURE = True
-#     SECURE_BROWSER_XSS_FILTER = True
-#     SECURE_CONTENT_TYPE_NOSNIFF = True
