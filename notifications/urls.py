c3e933a0655c966434e7decfa8c4d5b30ebe7e8c
josseycodes1 <adewumijosephine1@gmail.com>
2025-11-12 19:52:30 +0100

    Feat:Routing - Changed the notifications url to be more unique

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notifications/', views.create_notification, name='create-notification'),
-    path('notifications/', views.list_notifications, name='list-notifications'),
+    path('notifications/', views.create_notification, name='create-notification'),  # POST
+    path('notifications/', views.list_notifications, name='list-notifications'),    # GET 
     path('notifications/<str:notification_type>/status/', views.update_notification_status, name='update-status'),
 ]
